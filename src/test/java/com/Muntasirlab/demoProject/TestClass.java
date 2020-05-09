6677bf5e9c893ce0a1bb34b964b7b859d61981c7
MAJilani <muntasir.ahmad.jilani@gmail.com>
2020-05-09 16:07:31 +0530

    My first commit

---
+++ src/test/java/com/Muntasirlab/demoProject/TestClass.java
@@ -0,0 +1,45 @@
+package com.Muntasirlab.demoProject;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+
+public class TestClass {
+
+	public static WebDriver driver;
+	
+	@BeforeMethod
+	public void launchDriver() {
+		System.setProperty("webdriver.chrome.driver", "C:\\PayPal backup\\mavendemo\\mavendemo\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+		
+	}
+	
+	@Test
+	public void Test1() {
+		driver.navigate().to("https://google.com");
+		System.out.println("Test 1 title is"+driver.getTitle());
+	}
+	@Test
+	public void Test2() {
+		
+		driver.navigate().to("https://google.com");
+		System.out.println("Test 2 title is"+driver.getTitle());
+	}
+	@Test
+	public void Test3() {
+		driver.navigate().to("https://google.com");
+		System.out.println("Test 3 title is"+driver.getTitle());
+	
+    }
+	
+	@AfterMethod
+	public void quit() {
+		driver.quit();
+	}
+}
